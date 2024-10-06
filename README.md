f0171745a4ac8c99d249cd0409b752e978232e17
kaichiachen <kaichiaboy@gmail.com>
2024-10-06 19:14:43 +0800

    Update Readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Voice Control Over Google Gemini LLM
+
+Voice Control Over Google Gemini LLM is a free add-on for Home Assistant that allows you to control devices using Google Gemini's language model.
+
+## Configuration
+
+1. **Google Gemini Token**: Enter your Google Gemini API token. You can apply for one [here](https://aistudio.google.com/app/apikey).
+2. **Home Assistant Token**: Enter your Home Assistant token. You can find it under **Your Profile** -> **Security** -> **Refresh Token**. For more details, refer to the [Authentication Page](https://www.home-assistant.io/docs/authentication/).
+
+   ![Configuration](assets/configuration.jpg)
+
+3. **Integrate with Home Assistant**: Configure the add-on to work with your Home Assistant conversation agent.
--- custom_components/voice_control_over_llm/langchainMgr.py
+++ custom_components/voice_control_over_llm/langchainMgr.py
@@ -15,20 +15,26 @@ class Entity(BaseModel):
     state: str = Field(description="state of entity")
 
 class Switch(Entity):
-    action: str = Field(description="action for the device. Only can be on/off/other. Default value is other")
+    action: str = Field(description="action for the device from {user_input}. Only can be on/off/other. Default value is other")
 
 class Fan(Switch):
-    percentage: float = Field(description="the percentage or number represent the level of air volume that user to set. Only for fan. Default value is -1 if no mention")
+    percentage: float = Field(description="the percentage or number represent the level of fan air volume from {user_input}. Set value as -1 if no mention from {user_input}")
+
+class Climate(Switch):
+    temperature: float = Field(description="the temperature represent air condition's target temperature from {user_input}. Set value as -1 if no mention from {user_input}")
+    fan_mode: str = Field(description="fan mode of air condition. Only can be auto/low/medium/high/other Default value is other")
+    hvac_mode: str = Field(description="HVAC(Heating, Ventilation, and Air Conditioning) of air condition. Only can be heat_cool/cool/dry/fan_only/heat/other Default value is other")
 
 class HomeAssistant(BaseModel):
     switch: List[Switch]
     fan: List[Fan]
-    response: str = Field(description="Response text accroding to {user_input} with maximum 100 words and minium 2 words. Answer in plain text. Keep it simple to the point. Please do not include any text formatting and reply with traditional chinese")
+    climate: List[Climate]
+    response: str = Field(description="Response text accroding to {user_input} with maximum 100 words and minium 2 words. Answer in plain text. Keep it simple to the point. Please do not include any text formatting and reply with language of {user_input}. If language is chinese, should identify traditional chinese or simple chinese and traditional chinese first if unidentifiable")
 
 class LangchainMgr:
 
-     def __init__(self, api_key: str, 
-                  hass: HomeAssistantStub, url: str, 
+     def __init__(self, api_key: str,
+                  hass: HomeAssistantStub, url: str,
                   headers: Dict[str, str]) -> None:
         self.api_key = api_key
         self.hass = hass
@@ -36,13 +42,14 @@ class LangchainMgr:
         self.headers = headers
         self.parser = PydanticOutputParser(pydantic_object=HomeAssistant)
         self.llm = None
+        self.chain= None
 
      def invoke(self, user_input: str) -> Any:
         if self.chain is None:
             raise Exception("Chain is not initialized")
         response = self.chain.invoke({"user_input": user_input})
         return response
-    
+
      def getLLM(self) -> ChatGoogleGenerativeAI:
           safety_settings = {
                 HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
@@ -52,7 +59,7 @@ class LangchainMgr:
                 HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
           }
           return ChatGoogleGenerativeAI(model="gemini-1.5-flash-latest",
-                                                safety_settings=safety_settings, 
+                                                safety_settings=safety_settings,
                                                 google_api_key=self.api_key)
 
      async def update(self) -> None:
@@ -61,15 +68,15 @@ class LangchainMgr:
 
           prompt = PromptTemplate(
              template = """
-             You are the voice assistant for Home Assistant.
-             An overview of the areas and the devices in this smart home with format (entity name: entity_id) :\n {entity_map} \n
-             Extract the following information {format_instructions} from the input text: "{user_input}"
+             You are the voice assistant for Home Assistant but be able to response other common questions.
+             An overview of the areas and the devices in this smart home with entity_map format (entity name: entity_id) :\n {entity_map} \n\n\n
+             Extract the following information {format_instructions} from the input text: "{user_input} and entity_map"
              """,
              input_variables = ['user_input'],
              partial_variables={
                  "format_instructions": self.parser.get_format_instructions(),
                  "entity_map": json.dumps(entity_map)},
-        )
+          )
 
           if self.llm is None:
               self.llm = await self.hass.async_add_executor_job(self.getLLM)
@@ -80,7 +87,7 @@ class LangchainMgr:
 
 class OpRunner:
 
-    def __init__(self, headers: Dict[str, str], url: str, hass: HomeAssistant) -> None:
+    def __init__(self, headers: Dict[str, str], url: str, hass: HomeAssistantStub) -> None:
         self.headers = headers
         self.url = url
         self.hass = hass
@@ -105,45 +112,81 @@ class OpRunner:
         endpoint = self.url+'/api/services/'+device+'/set_percentage'
         await self.hass.async_add_executor_job(self.postCmd, endpoint, data)
 
+    async def setHVAC(self, entity_id: str, hvac_mode: str) -> None:
+        if hvac_mode not in ('heat_cool', 'cool', 'dry', 'fan_only', 'heat'):
+            return
+        data = {'entity_id': entity_id, 'hvac_mode': hvac_mode}
+        endpoint = self.url+'/api/services/climate/set_hvac_mode'
+        await self.hass.async_add_executor_job(self.postCmd, endpoint, data)
+
+
+    async def setFan(self, entity_id: str, fan_mode: str) -> None:
+        if fan_mode not in ('auto', 'low', 'medium', 'high'):
+            return
+        data = {'entity_id': entity_id, 'fan_mode': fan_mode}
+        endpoint = self.url+'/api/services/climate/set_fan_mode'
+        await self.hass.async_add_executor_job(self.postCmd, endpoint, data)
+
+
+    async def setTemperature(self, entity_id: str, temperature: float) -> None:
+        data = {'entity_id': entity_id, 'temperature': min(temperature, 100)}
+        endpoint = self.url+'/api/services/climate/set_temperature'
+        await self.hass.async_add_executor_job(self.postCmd, endpoint, data)
+
+
     async def runOps(self, op: HomeAssistant) -> str:
         switchs = op.switch
+        for switch in switchs:
+            if switch.state != switch.action:
+                await self.switchAction(switch.entity_id, switch.action)
+
         fans = op.fan
         for fan in fans:
             if fan.state != fan.action:
                 await self.switchAction(fan.entity_id, fan.action)
-            if fan.percentage >= 0:
+            if fan.percentage >= 0 and fan.action != 'other':
                 await self.setPercentage(fan.entity_id, 'fan', fan.percentage)
+
+        climates = op.climate
+        for climate in climates:
+            if climate.state != climate.action:
+                await self.switchAction(climate.entity_id, climate.action)
+            if climate.action != 'other':
+                await self.setHVAC(climate.entity_id, climate.hvac_mode)
+                await self.setFan(climate.entity_id, climate.fan_mode)
+                await self.setTemperature(climate.entity_id, climate.temperature)
 
         resp = op.response
         return resp
 
 
 async def async_get_devices_info(
-          hass: HomeAssistant, 
-          dns: str, 
+          hass: HomeAssistantStub,
+          dns: str,
           headers: Dict[str, str]) -> List[Dict[str, Any]]:
-     
+
      def query_device_info() -> requests.Response:
           url = f"{dns}/api/states"
           response = requests.get(url, headers=headers)
           return response
 
      resp = await hass.async_add_executor_job(query_device_info)
-     
+
      states: List[Dict[str, Any]] = []
-     
+
      def handle_entity(entity: Dict[str, Any]) -> None:
           states.append(entity)
-          
+
      support_entities = {
           "light": handle_entity,
           "switch": handle_entity,
           "fan": handle_entity,
           "vacuum": handle_entity,
           "input_boolean": handle_entity,
-          "sensor": handle_entity
+          "sensor": handle_entity,
+          "climate": handle_entity,
      }
-     
+
      try:
           for item in resp.json():
                 k = item['entity_id']
